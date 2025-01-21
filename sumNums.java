4ed737bf7471e79c4cb90017bf2689cdba8dde11
delloydb <lloydbrownjnr@gmail.com>
2025-01-21 15:56:59 +0300

    Create sumNums.java

---
+++ sumNums.java
@@ -0,0 +1,14 @@
+/*this program sums up two variables already initialised with a values and gives the result of the computation*/
+public class Main{
+    public static void main(String[] args){
+        //initialising variables to be added
+        int x=5;
+        int y=6;
+        int sum;
+        //using variable sum to add 
+        sum=x+y;
+        //this outputs the result of the variables being added
+        System.out.println(+sum);
+        }
+        //end of program
+}
