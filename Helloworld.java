fa570c442d73c8a258babdbd4b5f71026fdd22f5
delloydb <lloydbrownjnr@gmail.com>
2025-01-21 15:46:34 +0300

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+//first program by LLOYD BROWN CT101/G/20148/23 PRINTING statement hello world
+public class Main{
+  //class made main
+    public static void main(String[] args){
+        System.out.println("Hello world");//printing command dont delete
+    }
+  //end of program
+}
